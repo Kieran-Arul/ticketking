0421e2e25d487b52da267923521839746b8a78e3
kieranarul <arul.kieran@gmail.com>
2024-12-04 02:11:22 +0000

    Initial commit

---
+++ ticketking-application/src/main/java/com/kieran/projects/ticketking/ticketkingapplication/TicketkingApplication.java
@@ -0,0 +1,13 @@
+package com.kieran.projects.ticketking.ticketkingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketkingApplication.class, args);
+    }
+
+}
---
+++ ticketking-application/src/test/java/com/kieran/projects/ticketking/ticketkingapplication/TicketkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kieran.projects.ticketking.ticketkingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
